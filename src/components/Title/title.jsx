b278c92ed31a2836321e55fb4a497f629c97017d
Vishal062 <vs9425348@gmail.com>
2021-12-11 13:28:03 +0530

    Home page update with contact

--- src/components/Title/title.jsx
+++ src/components/Title/title.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 import styled from "styled-components";
+//import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 
 const MyTitleMessage = styled.h1`
   position: absolute;
@@ -8,6 +13,7 @@ const MyTitleMessage = styled.h1`
   top: 22rem;
   z-index: 1;
   margin-top: -125px;
+  
   text-align: center;
   strong {
     font-size: 1.25em;
@@ -23,6 +29,7 @@ const MyTitleMessage = styled.h1`
     .sub {
       font-size: 27px;
       letter-spacing: 2px;
+      margin-bottom: 50px;
     }
   }
 `;
@@ -48,6 +55,52 @@ const Title = () => (
             }}
           />
         </div>
+        {/* <Jumbotron className="contact-jumbotron"> */}
+        <div className="LineHeight">
+        <Row>
+          <Col className="d-flex justify-content-center flex-wrap">
+            
+            <div className="m-2">
+              <a
+                href="mailto:vs9425348@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button
+                  variant="outline-danger"
+                  title="vs9425348@gmail.com"
+                >
+                  <i className="fas fa-envelope-square"></i> Email Me
+                </Button>
+              </a>
+            </div>
+
+            <div className="m-2">
+              <a
+                href="https://github.com/Vishal062"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="outline-light" title="My other projects">
+                  <i className="fab fa-github-square"></i> GitHub
+                </Button>
+              </a>
+            </div>
+            <div className="m-2 ">
+              <a
+                href="https://www.linkedin.com/in/iamvishalsinha/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="outline-primary" title="Visit my LinkenIn">
+                  <i className="fab fa-linkedin"></i> LinkedIn
+                </Button>
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      {/* </Jumbotron> */}
       </div>
     </div>
   </MyTitleMessage>
